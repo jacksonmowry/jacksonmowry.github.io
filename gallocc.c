15b7419d2475110b5242cde675609a464dddab3d
Jackson Mowry <jacksonm@duck.com>
2024-01-04 07:16:37 -0500

    Fixed heap_seg struct to enable walking forward/backwards

--- gallocc.c
+++ gallocc.c
@@ -5,20 +5,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define HEAP_INC 32
+#define HEAP_INC 128
 
 void *heap_base = NULL;
 void *heap_end = NULL;
 
-typedef struct {
+typedef struct heap_seg {
   size_t size;
+  struct heap_seg *prev;
 } heap_seg;
 
 void *malloc(size_t bytes) {
   if (!heap_base) {
     heap_base = sbrk(0);
     sbrk(HEAP_INC);
-    *(heap_seg*)heap_base = (heap_seg){HEAP_INC};
+    *(heap_seg*)heap_base = (heap_seg){HEAP_INC, NULL};
     heap_end = sbrk(0);
     /* printf("Diff: %ld\n", heap_end - heap_base); */
   }
@@ -27,6 +28,7 @@ void *malloc(size_t bytes) {
   }
 
   // Start from heap base, find first open segment that can hold 'bytes'
+  heap_seg *prev = NULL;
   heap_seg *ptr = heap_base;
   while (ptr < (heap_seg*)heap_end) {
     /* printf("Size is: %ld, Free?: %s\n", ptr->size ^ 0x1, */
@@ -36,38 +38,43 @@ void *malloc(size_t bytes) {
       size_t old_cap = ptr->size;
       // Heap segment is free, and big enough
       ptr->size = sizeof(heap_seg) +
-                      ((bytes + (sizeof(heap_seg) - 1)) / sizeof(heap_seg)) *
-                          sizeof(heap_seg) |
+                      ((bytes + (sizeof(void*) - 1)) / sizeof(void*)) *
+                          sizeof(void*) |
                   0x1;
+      ptr->prev = prev;
       void *user_block = ptr + 1;
       size_t next_size = old_cap - (ptr->size ^ 0x1);
       if (next_size <= sizeof(heap_seg)) {
         ptr->size += next_size;
       } else if (old_cap > ptr->size) {
-        heap_seg *next_block = ptr + ptr->size / sizeof(heap_seg);
+        heap_seg *next_block = (void*)ptr + (ptr->size & (~1));
         next_block->size = old_cap - (ptr->size ^ 0x1);
+        next_block->prev = prev;
         /* printf("next blocks size: %ld\n", next_block->size); */
       }
       return user_block;
     }
-    ptr += ptr->size / sizeof(heap_seg);
+    prev = ptr;
+    ptr = (void*)ptr + (ptr->size & (~1));
   }
 
   // Ran out of space
   /* puts("RESIZING"); */
   size_t increment = (bytes > HEAP_INC) ?
-    sizeof(heap_seg) + ((bytes + (sizeof(heap_seg)-1)) / sizeof(heap_seg)) * sizeof(heap_seg)
+    sizeof(heap_seg) + ((bytes + (sizeof(void*)-1)) / sizeof(void*)) * sizeof(void*)
     : HEAP_INC;
   sbrk(increment);
   heap_end = sbrk(0);
-  size_t new_size = sizeof(heap_seg) + ((bytes+(sizeof(heap_seg)-1))/sizeof(heap_seg))*sizeof(heap_seg)|0x1;
+  size_t new_size = sizeof(heap_seg) + ((bytes+(sizeof(void*)-1))/sizeof(void*))*sizeof(void*)|0x1;
   ptr->size = new_size;
+  ptr->prev = prev;
   size_t next_size = increment - (ptr->size ^ 0x1);
   if (next_size <= sizeof(heap_seg)) {
     ptr->size += next_size;
   } else if (bytes < HEAP_INC) {
-    heap_seg *next_block = ptr + ptr->size / sizeof(heap_seg);
+    heap_seg *next_block = (void*)ptr + (ptr->size & (~1));
     next_block->size = next_size;
+    next_block->prev = prev;
     /* printf("next blocks size: %ld\n", next_block->size); */
   }
   return ptr + 1;
@@ -79,10 +86,8 @@ void free(void* block) {
   if (!block) { return; }
   heap_seg *header = (heap_seg*)block - 1;
   // Get prev/next pointers
-  heap_seg *prev = header - header->size / sizeof(heap_seg);
-  heap_seg *next = header + header->size / sizeof(heap_seg);
-  printf("Prev: %p\n", prev);
-  printf("Next: %p\n", next);
+  heap_seg *prev = header->prev;
+  heap_seg *next = (void*)header + (header->size & (~1));
   if (prev > (heap_seg*)heap_base && !(prev->size & 1)) {
     prev->size += header->size;
     header = prev;
@@ -90,7 +95,6 @@ void free(void* block) {
   if (next < (heap_seg*)heap_end && !(next->size & 1)) {
     header->size += next->size;
   }
-  printf("Header Address: %p\n", header);
   header->size ^= 1;
 }
 
@@ -98,57 +102,27 @@ void heap_size() {
   printf("Heap is %ld bytes\n", heap_end - heap_base);
 }
 
+void heap_walk() {
+  heap_seg* ptr = heap_base;
+  while (ptr < (heap_seg*)heap_end) {
+    printf("Segment size: %4ld, Prev: %14p, Value: %ld, Address: %p\n", ptr->size, ptr->prev, (ptr->size & 1) ? *(uint64_t*)(ptr+1) : -1, ptr);
+    ptr = (void*) ptr + (ptr->size & (~1));
+  }
+}
+
 int main() {
   int *block = malloc(4);
   *block = 4;
-  heap_size();
 
-  int *num = malloc(4);
+  uint64_t *num = malloc(8);
   *num = 7;
-  heap_size();
 
-
-  int *test = malloc(1);
+  uint64_t *test = malloc(69);
   *test = 66;
-  heap_size();
-
-  printf("Address of block: %p\n", block);
-  printf("Address of num: %p\n", num);
-  printf("Address of test: %p\n", test);
-
-  printf("Size of block: %ld\n", ((heap_seg*)block-1)->size ^ 0x1);
-  printf("Size of num: %ld\n", ((heap_seg*)num-1)->size ^ 0x1);
-  printf("Size of test: %ld\n", ((heap_seg*)test-1)->size ^ 0x1);
-
-  free(num);
-  free(test);
-
-  heap_seg* ptr = heap_base;
-  while (ptr < (heap_seg*)heap_end) {
-    printf("Segment size: %ld, Value: %ld, Address: %p\n", ptr->size, (ptr->size & 1) ? *(uint64_t*)(ptr+1) : -1, ptr);
-    ptr += ptr->size / sizeof(heap_seg);
-  }
-  exit(1);
-
-  num = malloc(32);
-  *num = 1;
-  heap_size();
 
   uint64_t *big_num = malloc(64);
   *big_num = 64;
-  printf("address of big_num: %p\n", big_num);
-  printf("big_num: %ld\n", *big_num);
-  heap_size();
 
-  printf("Address of block: %p\n", block);
-  printf("Address of num: %p\n", num);
-  printf("Size of block: %ld\n", ((heap_seg*)block-1)->size ^ 0x1);
-  printf("Size of num: %ld\n", ((heap_seg*)num-1)->size ^ 0x1);
-
-
-
-
-
-
+  heap_walk();
   return 0;
 }
