cc14e67ea2f6ca5e865f9ecf4bfa440b1157dc74
Jackson Mowry <jacksonm@duck.com>
2024-08-20 15:57:08 -0400

    refactor

--- xml_parser/jack_sparrow.c
+++ xml_parser/jack_sparrow.c
@@ -1,8 +1,59 @@
 #include "xml_parser.h"
+#include <assert.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+typedef struct field {
+    enum type { STRING, INTEGER, TIMESTAMP } type;
+    bool required;
+    bool array;
+    char* default_value;
+    char* name;
+} field;
+
+VECTOR_PROTOTYPE(field);
+VECTOR(field);
+
+typedef struct message {
+    char* name;
+    vector_field fields;
+} message;
+
+VECTOR_PROTOTYPE(message);
+VECTOR(message);
+
+typedef struct schema {
+    char* version;
+    vector_message messages;
+} schema;
+
+char* parse_opening_schema(element e) {
+    assert(e.type == OPENING_TAG);
+
+    // grab version, should be the only prop
+    kv_pair version_prop = plist_get_pair(e.properties, 0);
+    // Key must be "version"
+    if (!strcmp("version", version_prop.key)) {
+        fprintf(stderr,
+                "%s:%s:%d: tag '<schema>' must have exactly "
+                "one property 'version'\n",
+                __FILE__, __func__, __LINE__);
+        exit(1);
+    }
+
+    // Check to ensure that we have a version number
+    if (version_prop.val == NULL || strlen(version_prop.val) == 0) {
+        fprintf(stderr,
+                "%s:%s:%d: Version string must be non-null and "
+                "non-zero length\n",
+                __FILE__, __func__, __LINE__);
+        exit(1);
+    }
+
+    return version_prop.val;
+}
 
 int main() {
     struct stat sb;
@@ -14,6 +65,38 @@ int main() {
 
     streaming_parser p = (streaming_parser){
         .input = file, .length = sb.st_size, .current = file[0]};
+
+    element_t e;
+    schema s = {};
+    message temp_message = {};
+    field temp_field = {};
+
+    while ((e = next_element(&p)).tag != END_OF_FILE) {
+        switch (e.tag) {
+        case EMPTY:
+            fprintf(stderr, "%s:%s:%d: Ran into an empty element, exiting\n",
+                    __FILE__, __func__, __LINE__);
+            exit(1);
+            break;
+        case ELEMENT: {
+            element element = e.e.e;
+            // Parse opening schema
+            switch (element.type) {
+            case OPENING_TAG: {
+                if (s.version == NULL &&
+                    !strcmp("schema", element.element_type)) {
+                    s.version = parse_opening_schema(element);
+                }
+            } break;
+            case CLOSING_TAG:
+                break;
+            }
+        } break;
+        CLOSING_TAG:
+        case TEXT_CONTENT:
+        case END_OF_FILE:
+        }
+    }
 
     print_document(&p);
 }
--- xml_parser/xml_parser.c
+++ xml_parser/xml_parser.c
@@ -18,52 +18,6 @@ VECTOR_PROTOTYPE(element_t);
 /***********************/
 /* generic vector impl */
 /***********************/
-#define VECTOR(type)                                                           \
-    void vector_##type##_pb(vector_##type* v, type item) {                     \
-        if (v->len >= v->cap) {                                                \
-            v->cap *= 2;                                                       \
-            v->array = (type*)realloc(v->array, v->cap * sizeof(type));        \
-        }                                                                      \
-        v->array[v->len++] = item;                                             \
-    }                                                                          \
-    type vector_##type##_get(vector_##type* v, size_t index) {                 \
-        if (index >= v->len) {                                                 \
-            fprintf(stderr,                                                    \
-                    "Attempt to access memory oob for v[%zu], index %zu\n",    \
-                    v->len, index);                                            \
-            exit(1);                                                           \
-        }                                                                      \
-        return v->array[index];                                                \
-    }                                                                          \
-    vector_##type vector_##type##_init(size_t cap) {                           \
-        return (vector_##type){.array = (type*)calloc(cap, sizeof(type)),      \
-                               .cap = cap};                                    \
-    }                                                                          \
-    vector_##type vector_##type##_from(const type* const input, size_t size) { \
-        vector_##type blank = vector_##type##_init(size);                      \
-        blank.len = size;                                                      \
-        memcpy(blank.array, input, sizeof(type) * size);                       \
-        return blank;                                                          \
-    }                                                                          \
-    bool vector_##type##_eq(vector_##type a, vector_##type b,                  \
-                            bool (*equality_func)(void*, void*)) {             \
-        if (a.len != b.len) {                                                  \
-            return false;                                                      \
-        }                                                                      \
-        for (size_t i = 0; i < a.len; i++) {                                   \
-            if (!equality_func(&a.array[i], &b.array[i])) {                    \
-                return false;                                                  \
-            }                                                                  \
-        }                                                                      \
-        return true;                                                           \
-    }                                                                          \
-    bool vector_##type##_cmp(vector_##type a, vector_##type b) {               \
-        if (a.len != b.len) {                                                  \
-            return false;                                                      \
-        }                                                                      \
-        return !memcmp(a.array, b.array, sizeof(type) * a.len);                \
-    }                                                                          \
-    void vector_##type##_free(vector_##type v) { free(v.array); }
 
 VECTOR(char);
 VECTOR(string);
